a102186a6c8c85952c8ae20459e1e93b51d47fe5
GahwonLee <creativitRy@gmail.com>
2018-09-15 17:22:03 -0500

    switching bullet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,43 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+	Rigidbody2D rb;
 
-    Rigidbody2D rb;
-    [SerializeField]float speed = 20;
-    [SerializeField] LifeHearts lifeBar;
-    public int startinghealth = 50;
-    public int currenthealth;
-    bool isDead;
+	[SerializeField]
+	float speed = 20;
+
+	[SerializeField]
+	LifeHearts lifeBar;
+
+	public int startinghealth = 50;
+	public int currenthealth;
+	bool isDead;
 
 	// Use this for initialization
-	void Start () {
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        currenthealth = startinghealth;
-        lifeBar.updateLifeUI(currenthealth);
-        isDead = false;
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		currenthealth = startinghealth;
+		lifeBar.updateLifeUI(currenthealth);
+		isDead = false;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed,
-            Input.GetAxis("Vertical") * speed, 0);
-      
+	void Update()
+	{
+		rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed,
+			Input.GetAxis("Vertical") * speed, 0);
 	}
 
-    public void damage (int amount)
-    {
-        currenthealth -= amount;
-        if(currenthealth <= 0 && !isDead)
-        {
-            isDead = true;
-            
-        }
-        lifeBar.updateLifeUI(currenthealth);
-    }
-    private void FixedUpdate()
-    {
-        
-    }
+	public void damage(int amount)
+	{
+		currenthealth -= amount;
+		if (currenthealth <= 0 && !isDead)
+		{
+			isDead = true;
+		}
+
+		lifeBar.updateLifeUI(currenthealth);
+	}
+
+	private void FixedUpdate()
+	{
+	}
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	public static Player Instance { get; private set; }
 }
