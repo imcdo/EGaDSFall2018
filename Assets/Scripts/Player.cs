1951da67e1c0ce1361d52282bfed03fd8c3b5672
Abby469 <abbyritchie469@gmail.com>
2018-09-15 15:26:11 -0500

    Add Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rb;
+    [SerializeField]float speed = 20;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed,
+            Input.GetAxis("Vertical") * speed, 0);
+
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
