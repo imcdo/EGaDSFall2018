ca8b577380093448268c48acb980f881e9b92537
Abby469 <abbyritchie469@gmail.com>
2018-09-15 23:20:24 -0500

    add exploding bullet

---
+++ Assets/Scripts/BulletPattern/ExplodingBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bullet;
+
+public class ExplodingBullet : MonoBehaviour
+    {
+
+    public float duration = 1;
+    public int numBullets = 16;
+    public BulletType Bullet;
+    float counter = 0f;
+    public float speed = 5.0f;
+    public float angle = 0f;
+    private void Start()
+    {
+        
+    }
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float delta = (360f / numBullets);
+        counter += Time.deltaTime;
+        if (counter > duration)
+        {
+            counter = float.NegativeInfinity;
+
+            for (int i = 1; i <= numBullets; i++)
+            {
+                float angle= i * delta;
+                BaseBullet.Create(Bullet, transform.position, angle);
+            }
+        }
+                
+	}
+}
