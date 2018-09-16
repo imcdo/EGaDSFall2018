57ab784e4daba10903a9bf7214870944571f1e95
Abby469 <abbyritchie469@gmail.com>
2018-09-15 22:23:31 -0500

    shield on player

---
+++ Assets/Scripts/Lawnmower.cs
@@ -0,0 +1,35 @@
+﻿using CloudCanards.Util;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lawnmower : MonoBehaviour
+{
+    public float Speed = 4;
+
+    void Start()
+    {
+        var angle = transform.eulerAngles.z;
+
+        var body = GetComponent<Rigidbody2D>();
+        body.velocity = Vector2Utils.CreateVector(Speed, angle * Mathf.Deg2Rad);
+
+
+    }
+
+}
+
+
+
+   
+
+    
+   
+
+
+
+
+
+
+	
+	
--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Shield : MonoBehaviour {
 
     Vector3 mouse_pos;
-    public Transform target;
+    public new Transform transform;
     Vector3 object_pos;
     float angle;
 
@@ -22,11 +22,11 @@ public class Shield : MonoBehaviour {
     {
         mouse_pos = Input.mousePosition;
         mouse_pos.z = 0;
-        object_pos = Camera.main.WorldToScreenPoint(target.position);
+        object_pos = Camera.main.WorldToScreenPoint(base.transform.position);
         mouse_pos.x = mouse_pos.x - object_pos.x;
         mouse_pos.y = mouse_pos.y - object_pos.y;
         angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        base.transform.rotation = Quaternion.Euler(0, 0, angle);
     
 	}
 }
