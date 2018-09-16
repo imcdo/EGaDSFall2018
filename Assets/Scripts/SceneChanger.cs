1b408ee532ace3f8631d56c7faab57e93619fb26
imcdo <ian_mcdonald@rocketmail.com>
2018-09-16 15:29:18 -0500

    add prefabs

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+    [SerializeField] int level = 1;
+    public int NumToKill = 1;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ChangeLevel(string Level)
+    {
+        SceneManager.LoadScene("Victory");
+    }
+
+    public void CheckLevelOver()
+    {
+        if (NumToKill == 0)
+        {
+            SceneManager.LoadScene("Level" + level);
+        }
+    }
+}
